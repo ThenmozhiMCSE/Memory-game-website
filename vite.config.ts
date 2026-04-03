e9b18d16c385d6cfd0187ba6485c78565bf955fa
Thenmozhi <thenmozhicse2003@gmail.com>
2026-04-03 14:32:46 +0530

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Memory-game-website/',  // 👈 keep this
+  base: '/',   // 👈 FIXED
   server: {
     host: true
   },
