58947e9e8618d73cbd9228c16794da707855a4ef
Thenmozhi <thenmozhicse2003@gmail.com>
2026-04-03 15:15:21 +0530

    fixed vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // 👈 FIXED
+  base: '/', // ✅ correct for Vercel
   server: {
     host: true
   },
